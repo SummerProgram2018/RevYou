bae42f52cd3c671907723de8bb78aceeaca264b6
aXises <leletonyli.13944@yahoo.com.au>
2018-07-05 01:01:47 +1000

    Remove compiled files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-exports.router = express.Router();
-exports.router.get("/", (req, res, next) => {
-    res.render("index", {
-        "title": "RevYou"
-    });
-});
