5b6f4f4ab536b31a463e938b9d75b284e38cf50f
aXises <leletonyli.13944@yahoo.com.au>
2018-07-09 00:01:11 +1000

    Reformat review and reviewfield class

--- src/Review.ts
+++ src/Review.ts
@@ -1,29 +1,17 @@
-import * as fs from "fs";
+
 import { ReviewField } from "ReviewField"; 
+import { User } from "User";
 
 export class Review {
     public readonly id: string;
-    public readonly overall: ReviewField; // If we use this, the title for this one must be "Overall".
-    public readonly categories: Array<ReviewField>; //This one's for having a bunch of sub-fields (eg. Sound, Gameplay). User specified
-
-    public constructor(id: string, overall: ReviewField, categories: Array<ReviewField>) {
+    public readonly content: ReviewField; // If we use this, the title for this one must be "Overall".
+    // This one's for having a bunch of sub-fields (eg. Sound, Gameplay). User specified
+    public readonly categories: ReviewField[];
+    public readonly writer: User;
+    public constructor(id: string, content: ReviewField, categories: ReviewField[], writer: User) {
         this.id = id;
-        this.overall = overall;
+        this.content = content;
         this.categories = categories;
-    }
-
-    public getReview(): Review {
-        return this;
-    }
-
-    public retrieveDb(id: string) {
-        return;
-    }
-
-    public saveReview(path: string) {
-        const content = JSON.stringify(this);
-        fs.writeFile(path, content, "utf8", (err: any) => {
-            //
-        });
+        this.writer = writer;
     }
 }
--- src/ReviewField.ts
+++ src/ReviewField.ts
@@ -1,32 +1,20 @@
 export class ReviewField {
-
-    public readonly id: string; //Dunno what the fug to do here
-    public readonly title: string; //eg. "Gameplay", "Graphics", "Sound", "Overall"
-    public readonly description: string; //List of thoughts and opinions on the product
-    public readonly rating: number; //Restrict this to be a number from 0-100, or 0-5. Whatever works.
-    public readonly date: Date; //Date this review was generated. Technically shows edit dates if that gets done, since edit history is just a copy of the old version
-
+    public readonly title: string; // eg. "Gameplay", "Graphics", "Sound", "Overall"
+    public readonly description: string; // List of thoughts and opinions on the product
+    public readonly rating: number; // Restrict this to be a number from 0-100, or 0-5. Whatever works.
+    // Date this review was generated. Technically shows edit dates if that gets done,
+    // since edit history is just a copy of the old version
+    public readonly date: Date;
     private readonly ratingMin = 0;
     private readonly ratingMax = 100;
-
-    public constructor(id: string, title: string: description: string, rating: number, date: Date) {
-        this.id = id;
+    public constructor(id: string, title: string, description: string, rating: number, date: Date) {
         this.title = title;
         this.description = description;
         if (this.ratingMin <= rating && rating <= this.ratingMax) {
             this.rating = rating;
         } else {
-            //TODO: throw error
+            // TODO: throw error
         }
         this.date = date;
     }
-
-    public getReviewField(): ReviewField {
-        return this;
-    }
-
-    public retrieveDb(id: string) {
-        return;
-    }
-
 }
