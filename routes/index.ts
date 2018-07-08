edfcdf747d6338ccb9ee4a860beddd6cf8d70776
aXises <leletonyli.13944@yahoo.com.au>
2018-07-09 01:45:56 +1000

    Formatting

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,15 @@
 import * as express from "express"
 export const router = express.Router();
+import * as express from "express";
+import { UserDatabase } from "../src/UserDatabase";
+import { User } from "../src/User";
 
+export const router = express.Router();
+const x = new UserDatabase("src/data/user.json");
+const u = new User("egg2", "pass");
+x.addUser(u);
 router.get("/", (req, res, next) => {
     res.render("index", {
-        "title": "RevYou"
+        title: "RevYou"
     });
 });
