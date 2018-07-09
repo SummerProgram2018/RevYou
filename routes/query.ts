b33f5f4acf5bb1365e9c80366d5b9d11f3977280
aXises <leletonyli.13944@yahoo.com.au>
2018-07-10 00:26:37 +1000

    Allow query between product types

--- routes/query.ts
+++ routes/query.ts
@@ -1,17 +1,33 @@
 import * as express from "express";
 import { ProductDatabase } from "../src/ProductDatabase";
+import { randInt } from "../src/Utils";
+import { Product } from "../src/Product";
 export const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    console.log(req.query);
-    const dataSet = new ProductDatabase("src/data/games.json");
-    // for (let i of x) {
-    //     console.log(i)
-    //     if(i.name==req.query.query)
-    //         newX.push(i);
-    // }
-    console.log("m", dataSet.getData());
+    const data: object = {
+        games: new ProductDatabase("src/data/games.json"),
+        movies: new ProductDatabase("src/data/movies.json")
+        // books: new ProductDatabase("src/data/books.json")
+    };
+    let dataSet: any[] = [];
+    if (!req.query.productType || req.query.productType === "anything") {
+        for (let i = 0; i < 100; i++) {
+            const set: number = randInt(0, Object.keys(data).length);
+            const products: Product[] = data[Object.keys(data)[set]].getData();
+            const selection: Product = products[randInt(0, products.length)];
+            if (dataSet.findIndex((e: Product) => e.name === selection.name) === -1) {
+                dataSet.push(selection);
+            }
+        }
+    } else {
+        dataSet = data[req.query.productType].getData();
+        if (!dataSet) {
+            dataSet = [];
+        }
+    }
     res.render("query", {
-        array: dataSet.getData()
+        results: dataSet,
+        resultLength: dataSet.length
     });
 });
