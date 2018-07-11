93c981e5385503d8bb6146e8d9047251bee3121f
aXises <leletonyli.13944@yahoo.com.au>
2018-07-12 01:19:24 +1000

    Set up review page

--- routes/review.ts
+++ routes/review.ts
@@ -1,7 +1,30 @@
-import * as express from "express"
+import * as express from "express";
+import { ProductDatabase } from "../src/ProductDatabase";
+import { Product } from "../src/Product";
 export const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/:type/:id", (req, res, next) => {
+    const data: any = {
+        game: new ProductDatabase("src/data/games.json"),
+        movie: new ProductDatabase("src/data/movies.json"),
+        book: new ProductDatabase("src/data/books.json")
+    };
+    const dataSet = data[req.params.type.toLowerCase()].getData();
+    const index = dataSet.findIndex((e: Product) => Number(e.id) === Number(req.params.id));
+    let rating = Number(dataSet[index].score);
+    let activeRating = 0;
+    console.log(rating)
+    while (rating > 10) {
+        rating -= 10;
+        activeRating++;
+    }
+    const inactiveRating = 10 - rating;
+    console.log(activeRating)
     res.render("review", {
+        product: dataSet[index],
+        ratings: {
+            activeRating,
+            inactiveRating
+        }
     });
 });
--- src/Product.ts
+++ src/Product.ts
@@ -1,10 +1,14 @@
 import { Review } from "Review";
 
 export class Product {
+    public id: string;
     public name: string;
     public description: string;
     public reviews: Review[];
     public type: string;
+    public tracker: {
+        likes: number
+    };
     public constructor(name: string, description: string, type: string) {
         this.name = name;
         this.description = description;
