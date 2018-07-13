ffbaf18eb70504cf03de390b11605719a8b9814c
aXises <leletonyli.13944@yahoo.com.au>
2018-07-13 11:18:53 +1000

    Revert "Resolve conflicts"
    This reverts commit d6d3713f2499aeab9b92cbd4f2974dc9b81e5c5a.

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,6 @@
 import * as express from "express";
 import { UserDatabase } from "../src/UserDatabase";
-import { ProductDatabase } from "../src/ProductDatabase";
 import { User } from "../src/User";
-import { randInt } from "../src/Utils";
-import { Product } from "../src/Product";
 
 export const router = express.Router();
 const x = new UserDatabase("src/data/user.json");
@@ -12,8 +9,5 @@ x.addUser(u);
 router.get("/", (req, res, next) => {
     res.render("index", {
         title: "RevYou"
-        
     });
-    
-    
 });
